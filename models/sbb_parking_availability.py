2bfdd49ce75b02ac103db18ae5fe9d4ed1cc2329
Alexandru Maiereanu <alexandrumaiereanu@Alexandrus-MacBook-Pro.local>
2018-11-24 21:19:38 +0100

    sbb_parking_availability: send encoded station names

--- models/sbb_parking_availability.py
+++ models/sbb_parking_availability.py
@@ -4,7 +4,7 @@ import requests
 class ParkingAvailability:
 
     def __init__(self):
-        self.url = 'http://not/yet/filled/mate'
+        self.url = 'http://not/yet/filled/mate/availability/{stations}'
 
     def get_stations_availability(self, stations):
         response = self.make_request(stations)
@@ -13,8 +13,11 @@ class ParkingAvailability:
         return availability
 
     def make_request(self, stations):
-        params = {'stations': stations}
-        response = requests.get(self.url, params=params)
+        encoded = ""
+        for station in stations:
+            encoded += station + '#'
+        final_url = self.url.format(stations=encoded)
+        response = requests.get(final_url)
 
         return response.json()
 
