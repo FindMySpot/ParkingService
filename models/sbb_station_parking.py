a20cd415e17ba4e7f85e11893dd9c8187c302994
Matthew Brookes <mbrookes1304@gmail.com>
2018-11-24 16:13:13 +0100

    sbb_station_parking: extract methods for parsing json

--- models/sbb_station_parking.py
+++ models/sbb_station_parking.py
@@ -20,15 +20,26 @@ class SBBStationParking:
         return SBBStationParking._data
 
     @staticmethod
-    def get_parking_information_for_station(didok):
-        station_carparks = list(filter(lambda x: x["fields"].get("didok", "") == didok, SBBStationParking.get_data()))
-        parking_info = list(map(lambda x: {
-            "parkrail_anzahl": x["fields"].get("parkrail_anzahl", -1),
-            "parkrail_preis_tag": x["fields"].get("parkrail_preis_tag", -1),
-            "parkrail_preis_monat": x["fields"].get("parkrail_preis_monat", -1),
-        }, station_carparks))
+    def get_info_for_all_stations():
+        didoks = set(map(lambda x: x["fields"]["didok"], SBBStationParking.get_data()))
+        return {
+            "stations": list(map(lambda x: SBBStationParking.get_info_for_station(x), didoks))
+        }
+
+    @staticmethod
+    def get_info_for_station(didok):
+        station_infos = list(filter(lambda x: x["fields"].get("didok", "") == didok, SBBStationParking.get_data()))
+        parking_info = list(map(lambda x: SBBStationParking.extract_parking_info(x), station_infos))
         return {
             "didok": didok,
-            "coordinates": station_carparks[0]["geometry"]["coordinates"],
+            "coordinates": station_infos[0].get("geometry", {}).get("coordinates", []),
             "parking": parking_info,
-        } if len(station_carparks) > 0 else {}
+        } if len(station_infos) > 0 else {}
+
+    @staticmethod
+    def extract_parking_info(station):
+        return {
+            "parkrail_anzahl": station["fields"].get("parkrail_anzahl", -1),
+            "parkrail_preis_tag": station["fields"].get("parkrail_preis_tag", -1),
+            "parkrail_preis_monat": station["fields"].get("parkrail_preis_monat", -1),
+        }
