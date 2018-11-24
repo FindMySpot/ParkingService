1066d494efd98e5b05c9ae04aeab41104d995ea1
Matthew Brookes <mbrookes1304@gmail.com>
2018-11-24 15:46:14 +0100

    sbb_station_parking: get parking info for a station

--- models/sbb_station_parking.py
+++ models/sbb_station_parking.py
@@ -18,3 +18,17 @@ class SBBStationParking:
     def get_data():
         SBBStationParking._load_data()
         return SBBStationParking._data
+
+    @staticmethod
+    def get_parking_information_for_station(didok):
+        station_carparks = list(filter(lambda x: x["fields"].get("didok", "") == didok, SBBStationParking.get_data()))
+        parking_info = list(map(lambda x: {
+            "parkrail_anzahl": x["fields"].get("parkrail_anzahl", -1),
+            "parkrail_preis_tag": x["fields"].get("parkrail_preis_tag", -1),
+            "parkrail_preis_monat": x["fields"].get("parkrail_preis_monat", -1),
+        }, station_carparks))
+        return {
+            "didok": didok,
+            "coordinates": station_carparks[0]["geometry"]["coordinates"],
+            "parking": parking_info,
+        } if len(station_carparks) > 0 else {}
