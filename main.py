33685417457b557887d729c1faf4e390d61e7852
Matthew Brookes <mbrookes1304@gmail.com>
2018-11-24 16:32:03 +0100

    Add geo endpoints for stations

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({'Hello': 'world'})
+    return jsonify(SBBStationParking.get_parking_information_for_station(3004))
 
 
 @app.route('/data', methods=['GET'])
 def get_data():
     return jsonify(SBBStationParking.get_data())
 
+@app.route('/geo/stations', methods=['GET'])
+def get_geo_stations():
+    # Return all the stations contained in the data
+    return jsonify(SBBStationParking.get_geo_info_for_all_stations())
 
 @app.route('/stations', methods=['GET'])
 def get_stations():
